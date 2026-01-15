e097a5da91dc7f1dbbcde4e0799126f47b24d376
paulodev <pauloeduardotroll9646@gmail.com>
2026-01-15 12:11:59 -0300

    Organize Codeforces solutions by exercise

---
+++ A.NextRound/CF158A/NextRoud.java
@@ -0,0 +1,9 @@
+package CF158A;
+
+public class NextRoud {
+    //A resolution is for a problem 158A on CodeForces
+    //https://codeforces.com/problemset/problem/158/A
+    static void main(String[] args) {
+        System.out.println("vva");
+    }
+}
