0bf9aa85aa35770310a36ead715c86d0567f638c
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-18 19:06:28 -0300

    Remove NextRoud.java

--- A.NextRound/CF158A/NextRoud.java
+++
@@ -1,9 +0,0 @@
-package CF158A;
-
-public class NextRoud {
-    //A resolution is for a problem 158A on CodeForces
-    //https://codeforces.com/problemset/problem/158/A
-    static void main(String[] args) {
-        System.out.println("vva");
-    }
-}
