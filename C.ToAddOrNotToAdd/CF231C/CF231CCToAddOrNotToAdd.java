113534198cabf953b881218c721848838d4ec262
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-19 13:05:24 -0300

    feat:initialize solution structure for CF231C

---
+++ C.ToAddOrNotToAdd/CF231C/CF231CCToAddOrNotToAdd.java
@@ -0,0 +1,9 @@
+package CF231C;
+
+public class CF231CCToAddOrNotToAdd {
+    //A resolution is for a problem 231C on CodeForces
+    //https://codeforces.com/problemset/problem/231/C
+		public static void main(String[] args) throws Exception {
+		}
+	}
+
