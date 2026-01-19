905d53e60548eba721cd9816216797f56c4553d7
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-18 21:38:03 -0300

    CF158A: implement logic to count qualified participants

--- A.NextRound/CF158A/CF158ANextRoud.java
+++ A.NextRound/CF158A/CF158ANextRoud.java
@@ -12,26 +12,38 @@ public class CF158ANextRoud {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String initialData = bufferedReader.readLine();
         StringTokenizer initialTokenizer = new StringTokenizer(initialData, " ");
-        String nextToken;
+        String previousToken;
         int qualifiedParticipants = 0;
         int kthPosition = 0;
         int participantsCount = 0;
 
 
-              nextToken = initialTokenizer.nextToken();
-              participantsCount = Integer.parseInt(nextToken);
+              previousToken = initialTokenizer.nextToken();
+              participantsCount = Integer.parseInt(previousToken);
 
-              nextToken = initialTokenizer.nextToken();
-              kthPosition = Integer.parseInt(nextToken);
+              previousToken = initialTokenizer.nextToken();
+              kthPosition = Integer.parseInt(previousToken);
 
         String scoreLine = bufferedReader.readLine();
               StringTokenizer scoreTokenizer = new StringTokenizer (scoreLine, " ");
-        int [] scores = new int[participantsCount];
-        for (int j = 0;scoreTokenizer.hasMoreTokens(); j++) {
-            scores[j] = Integer.parseInt(scoreTokenizer.nextToken());
-        }
-        Arrays.stream(scores).sorted();
 
+
+              int position = 1;
+              int nextToken = 0;
+              int cutOffScore = 0;
+              StringTokenizer cutOffTokenizer = new StringTokenizer(scoreLine," ");
+              while (cutOffTokenizer.hasMoreTokens()) {
+            int score = Integer.parseInt(cutOffTokenizer.nextToken());
+            if (kthPosition == position)
+                cutOffScore = score;
+            position++;
+        }
+
+
+              while (scoreTokenizer.hasMoreTokens()) {
+                  nextToken = Integer.parseInt(scoreTokenizer.nextToken());
+                   qualifiedParticipants= (cutOffScore <= nextToken&& nextToken !=0)?qualifiedParticipants + 1 : qualifiedParticipants ;
+              }
         System.out.println(qualifiedParticipants);
     }
 }
