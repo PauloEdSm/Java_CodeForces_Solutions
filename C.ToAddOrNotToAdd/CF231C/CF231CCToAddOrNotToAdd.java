657ddf9c56de60a5b0dcbd3b1c3a6b14edb46b97
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-24 19:30:10 -0300

    Solve Codeforces 231C - To Add or Not to Add (Sliding Window/Sorting)

--- C.ToAddOrNotToAdd/CF231C/CF231CCToAddOrNotToAdd.java
+++ C.ToAddOrNotToAdd/CF231C/CF231CCToAddOrNotToAdd.java
@@ -9,31 +9,38 @@ public class CF231CCToAddOrNotToAdd {
     //A resolution is for a problem 231C on CodeForces
     //https://codeforces.com/problemset/problem/231/C
 		public static void main(String[] args) throws Exception {
-            int curretSum=0;
-            int maxFreq=0;
-            int maxValueOfOperation;
+            long sum=0;
+            int bestLen=1;
+            int bestFreq=0;
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
             String headLine = bufferedReader.readLine();
             StringTokenizer headerTokens = new StringTokenizer(headLine, " ");
             int n = Integer.parseInt(headerTokens.nextToken());
             int k = Integer.parseInt(headerTokens.nextToken());
+            int l = 0;
 
             String ValuesLine = bufferedReader.readLine();
             StringTokenizer valueTokenizer = new StringTokenizer(ValuesLine, " ");
             int[] nums = new int[n];
+            int bestValue = nums[0];
             for (int j = 0; valueTokenizer.hasMoreTokens(); j++) {
                 nums[j] = Integer.parseInt(valueTokenizer.nextToken());
             }
 
             Arrays.sort(nums);
-            maxValueOfOperation = nums[0];
-            for (int i=0,j = 0;i<nums.length&&curretSum<=curretSum+nums[i]-nums[j];i++ ,maxFreq++) {
-                while (i>j++||i==j++) {
-                    curretSum =+ nums[j] - nums[i];
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+                while ((long)(i - l + 1) * nums[i] - sum > k){
+                    sum -= nums[l];
+                    l++;
                 }
-                maxValueOfOperation = (maxValueOfOperation<=nums[j])?nums[j]:maxValueOfOperation;
+                int windowSize = i - l + 1;
+                if (windowSize > bestLen){
+                    bestLen = windowSize;
+                    bestValue = nums[i];
+                }
             }
-            System.out.println(maxFreq+" "+curretSum);
+            System.out.println(bestLen+" "+bestValue);
         }
 		}
