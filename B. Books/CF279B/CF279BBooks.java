95a071d43309a5ec1696e66036dd4ddb05675ae5
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-24 21:09:34 -0300

    feat:initialize solution structure for CF231C

---
+++ B. Books/CF279B/CF279BBooks.java
@@ -0,0 +1,8 @@
+package CF279B;
+
+public class CF279BBooks {
+    //A resolution is for a problem 279B on CodeForces
+    //https://codeforces.com/problemset/problem/279/B
+    public static void main(String[] args) {
+    }
+}
