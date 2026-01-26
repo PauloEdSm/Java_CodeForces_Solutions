baccc92f8d6b504bea648a317799c95ff10865e8
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-26 20:55:29 -0300

    save progress

--- B. Books/CF279B/CF279BBooks.java
+++ B. Books/CF279B/CF279BBooks.java
@@ -2,32 +2,38 @@ package CF279B;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class CF279BBooks {
     //A resolution is for a problem 279B on CodeForces
     //https://codeforces.com/problemset/problem/279/B
     public static void main(String[] args) throws Exception {
+        int dayCounter= 1;
+        int sum = 0;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader (System.in));
 
         String firstLineRead = bufferedReader.readLine();
         StringTokenizer firstLineReader = new StringTokenizer(firstLineRead, " ");
 
-        int totalBooks = Integer.parseInt(firstLineReader.nextToken());
-        int pagesPerDay = Integer.parseInt(firstLineReader.nextToken());
+        int numberOfBooks = Integer.parseInt(firstLineReader.nextToken());
+        int maxTime = Integer.parseInt(firstLineReader.nextToken());
 
         String lastLineRead = bufferedReader.readLine();
-        StringTokenizer lastLineReader = new StringTokenizer(firstLineRead, " ");
+        StringTokenizer lastLineReader = new StringTokenizer(lastLineRead, " ");
 
-        int [] readingTimePerBook = new int[totalBooks];
+        int [] readingTimePerBook = new int[numberOfBooks];
 
         for (int index= 0; lastLineReader.hasMoreTokens() && index < readingTimePerBook.length; index++) {
 
             readingTimePerBook[index] = Integer.parseInt(lastLineReader.nextToken());
 
         }
-        Arrays.stream(readingTimePerBook).forEach(System.out::println);
 
+        for (int start = 0; start < numberOfBooks; start++) {
+            for (int end = start;end<numberOfBooks;end++){
+
+            }
+            
+        }
     }
 }
