57ef2ece4a99f00a845b35f1a4909c9977096cd1
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-01-29 16:29:50 -0300

    Save Progress

--- B.Fence/CF363B/CF363BBFance.java
+++ B.Fence/CF363B/CF363BBFance.java
@@ -1,6 +1,25 @@
 package CF363B;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class CF363BBFance {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader (System.in));
+
+        String firstLineRead = bufferedReader.readLine();
+        StringTokenizer firstLineReader = new StringTokenizer(firstLineRead, " ");
+        int plankCount = Integer.parseInt(firstLineReader.nextToken());
+        int minWindowSum = Integer.parseInt(firstLineReader.nextToken());
+
+        String lastLineRead = bufferedReader.readLine();
+        StringTokenizer lastLineReader = new StringTokenizer(lastLineRead, " ");
+
+        int[] plankHeights = new int[plankCount];
+
+        for (int index = 0; index < plankCount;index++){
+            plankHeights[index] = Integer.parseInt(lastLineReader.nextToken());
+        }
     }
 }
